35440387838b2313c58334efc4002838261d1c42
janco.lonneker <jancoino@gmail.com>
2023-05-08 21:36:12 +0200

    v2

--- vdv_streamlit.py
+++ vdv_streamlit.py
@@ -38,7 +38,7 @@ if uploaded_file is not None:
                 content = readlines_from_file(menge_fzg_typ_path)
                 updated_content = add_new_line(content, new_id)
 
-                new_zip_path = update_zip(zip_ref, new_id, 1)
+                new_zip_path = update_zip(temp_path, new_id, 1)
                 print(new_zip_path)
 
             elif selected_function == 'Update Coordinates':

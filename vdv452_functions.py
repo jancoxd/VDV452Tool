1e3916f6c32f13d1a55bba2a339b2eabe9c0abdf
janco.lonneker <jancoino@gmail.com>
2023-05-09 21:03:04 +0200

    v2

--- vdv452_functions.py
+++ vdv452_functions.py
@@ -138,8 +138,8 @@ def get_stop_coordinates(zip_path):
 
             rec_ort_headers = [header.strip() for header in next(rec_ort_reader)]
             lid_verlauf_headers = [header.strip() for header in next(lid_verlauf_reader)]
-            st.write(str(rec_ort_headers.index))
-            st.write(str(lid_verlauf_headers.index))
+            st.write(rec_ort_headers.index)
+            st.write(lid_verlauf_headers.index)
 
             ort_nr_index = rec_ort_headers.index('ORT_NR')
             ort_pos_breite_index = rec_ort_headers.index('ORT_POS_BREITE')

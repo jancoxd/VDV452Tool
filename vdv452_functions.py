affd738f1990d5cfb3768f8c73c803303ae9a829
janco.lonneker <jancoino@gmail.com>
2023-05-09 22:10:07 +0200

    v2

--- vdv452_functions.py
+++ vdv452_functions.py
@@ -163,7 +163,7 @@ def get_stop_coordinates(zip_path):
 def get_routing(origin, destination, client):
     origin_lat, origin_lon = origin[1], origin[0]
     destination_lat, destination_lon = destination[1], destination[0]
-    client.directions(locations=[origin, destination], profile='bus')
+    route = client.directions(locations=[origin, destination], profile='bus')
     return [origin, destination, int(route.duration / 60), route.distance / 1000]
 
 
